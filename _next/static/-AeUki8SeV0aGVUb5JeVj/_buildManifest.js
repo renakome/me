a0f2fff5d25625142d7b9ea17f7597002869980a
renakome <renakome@users.noreply.github.com>
2026-01-18 15:10:54 +0000

    deploy: bb919448bd07d2b3b4403702a24d37c24499ad0a

---
+++ _next/static/-AeUki8SeV0aGVUb5JeVj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/879-7fe13540c35d48bd.js","static/chunks/pages/index-bd1495b547ca42ff.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
